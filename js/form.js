365391e0ef47336db76c730d889efd05384f28cc
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-08-13 19:26:06 +0700

    Синхронизация времени заезда и выезда

--- js/form.js
+++ js/form.js
@@ -1,6 +1,8 @@
 const mainForm = document.querySelector('.ad-form');
 const housingType = mainForm.querySelector('#type');
 const housingPrice = mainForm.querySelector('#price');
+const housingTimeIn = mainForm.querySelector('#timein');
+const housingTimeOut = mainForm.querySelector('#timeout');
 
 const MinPricesOfTypes = {
   'bungalow': 0,
@@ -16,6 +18,21 @@ const adjustPrice = () => {
   housingPrice.min = minValue;
 };
 
+const adjustTime = (evt, element) => {
+  for (let item of element.children) {
+    if (item.value === evt.target.value) {
+      item.setAttribute('selected', '');
+    } else {
+      item.removeAttribute('selected');
+    }
+  }
+};
+
+
 adjustPrice();
 housingType.addEventListener('change', adjustPrice);
-housingPrice.addEventListener('change', adjustPrice);
+housingTimeIn.addEventListener(('change'), (evt) => adjustTime(evt, housingTimeOut));
+housingTimeOut.addEventListener(('change'), (evt) => adjustTime(evt, housingTimeIn));
+
+
+
