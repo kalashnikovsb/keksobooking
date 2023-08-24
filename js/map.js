b984b418e675864dae865114fd9477c1662c1661
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-08-24 19:55:59 +0700

    Подключил leaflet

--- js/map.js
+++ js/map.js
@@ -5,3 +5,9 @@ const mapElement = document.querySelector('#map-canvas');
 
 const offers = getOfferObjects();
 const offerElements = getOfferElements(offers);
+
+L.map(mapElement)
+  .setView({
+    lat: 35.68,
+    lng: 139.7,
+  }, 10)
