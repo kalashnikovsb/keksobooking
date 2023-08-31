a3ef6710493dfd2f424c8ff1a01838ea7a86a9eb
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-09-01 00:03:21 +0700

    Синхронизация цены

--- js/form.js
+++ js/form.js
@@ -4,6 +4,12 @@ const housingPrice = mainForm.querySelector('#price');
 const housingTimeIn = mainForm.querySelector('#timein');
 const housingTimeOut = mainForm.querySelector('#timeout');
 const housingAddress = mainForm.querySelector('#address');
+const housingTitle = mainForm.querySelector('#title');
+
+const MIN_LENGTH_TITLE = 30;
+const MAX_LENGTH_TITLE = 1000;
+const MIN_PRICE = 0;
+const MAX_PRICE = 1000000;
 
 const MinPricesOfTypes = {
   'bungalow': 0,
@@ -32,5 +38,37 @@ adjustPrice();
 housingType.addEventListener('change', adjustPrice);
 housingTimeIn.addEventListener('change', adjustTime);
 housingTimeOut.addEventListener('change', adjustTime);
+
+
+housingPrice.addEventListener('input', (evt) => {
+  const minValue = MinPricesOfTypes[housingType.value];
+  const currentValue = evt.target.value;
+
+  if (currentValue < minValue || currentValue < MIN_PRICE) {
+    housingPrice.setCustomValidity(`Цена не может быть ниже ${minValue}`);
+  } else if (currentValue > MAX_PRICE) {
+    housingPrice.setCustomValidity(`Цена не может быть выше ${MAX_PRICE}`);
+  } else {
+    housingPrice.setCustomValidity('');
+  }
+
+  housingPrice.reportValidity();
+});
+
+
+housingTitle.addEventListener('input', () => {
+  const valueLength = housingTitle.value.length;
+
+  if (valueLength < MIN_LENGTH_TITLE) {
+    housingTitle.setCustomValidity(`Заголовок объявления должен содержать минимум ${MIN_LENGTH_TITLE} символов. Добавьте ${MIN_LENGTH_TITLE - housingTitle.value.length} символов`);
+  } else if (valueLength > MAX_LENGTH_TITLE) {
+    housingTitle.setCustomValidity(`Заголовок объявления должен содержать максимум ${MAX_LENGTH_TITLE} символов. Удалите ${housingTitle.value.length - MAX_LENGTH_TITLE} символов`);
+  } else {
+    housingTitle.setCustomValidity('');
+  }
+
+  housingTitle.reportValidity();
+});
+
 
 export {setAddress};
