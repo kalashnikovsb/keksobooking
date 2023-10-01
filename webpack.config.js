4a80a012b4ae286027a236984e90672eb332b837
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-10-01 22:41:41 +0700

    СТруктура проекта

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: './js/main.js',
+  devtool: 'source-map',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname),
   },
 };
