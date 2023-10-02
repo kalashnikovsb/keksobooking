3d51578b90faa9019859db4723d11618ff2ff8f4
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-10-02 19:23:30 +0700

    Создал папки build и source

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/main.js',
+  entry: './source/js/main.js',
   devtool: 'source-map',
   output: {
     filename: 'main.bundle.js',
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'build/js'),
   },
 };
